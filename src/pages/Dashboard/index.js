e8c78cecfbf2023d19a89f68821aef0d3a3fa80a
Romulo-Reis <romuloreistavares@hotmail.com>
2025-02-08 13:31:21 -0300

    feat(dashboard): adicionar listagem e ações para tickets
    Adiciona exibição de tickets no dashboard com título, tabela de chamados e ações para visualizar e editar.
    Inclui estilização com `dashboard.css` e novos ícones da biblioteca `react-icons/fi`.

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,6 +2,11 @@ import { useContext } from 'react';
 import {AuthContext} from '../../contexts/auth';
 
 import Header from '../../components/Header';
+import Title from '../../components/Title';
+import { FiPlus, FiMessageSquare, FiSearch, FiEdit2 } from 'react-icons/fi'; 
+
+import './dashboard.css';
+import { Link } from 'react-router-dom';
 
 export default function Dashboard(){
     const { logout } = useContext(AuthContext);
@@ -11,8 +16,68 @@ export default function Dashboard(){
     return (
         <div>
             <Header/>
-            <h1>Página Dashboard</h1>
-            <button onClick={handleLogout}>Sair da conta</button>
+            <div className="content">
+                <Title name="Tickets">
+                    <FiMessageSquare size={25} />
+                </Title>
+
+                <>
+                    <Link to="/new" className="new">
+                        <FiPlus color="#FFF" size={25} />
+                        Novo chamado                
+                    </Link>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th scope="col">Cliente</th>
+                                <th scope="col">Assunto</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Cadastrado em</th>
+                                <th scope="col">#</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td data-label="Cliente">Mercado Esquina</td>
+                                <td data-label="Assunto">Suporte</td>
+                                <td data-label="Status" >
+                                    <span className="badge" style={{backgroundColor: '#999'}}>
+                                        Em Aberto
+                                    </span>   
+                                </td>
+                                <td data-label="Cadastrado">01/02/2025</td>
+                                <td data-label="#">
+                                    <button className="action" style={{backgroundColor: '#3583f6'}}>
+                                        <FiSearch color='#FFF' size={17}/>
+                                    </button>
+                                    <button className="action" style={{backgroundColor: '#f6a935'}}>
+                                        <FiEdit2 color='#FFF' size={17}/>
+                                    </button>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td data-label="Cliente">Informática TECH</td>
+                                <td data-label="Assunto">Suporte</td>
+                                <td data-label="Status" >
+                                    <span className="badge" style={{backgroundColor: '#999'}}>
+                                        Em Aberto
+                                    </span>   
+                                </td>
+                                <td data-label="Cadastrado">01/02/2025</td>
+                                <td data-label="#">
+                                    <button className="action" style={{backgroundColor: '#3583f6'}}>
+                                        <FiSearch color='#FFF' size={17}/>
+                                    </button>
+                                    <button className="action" style={{backgroundColor: '#f6a935'}}>
+                                        <FiEdit2 color='#FFF' size={17}/>
+                                    </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </>
+            </div>
         </div>
     )
 }
